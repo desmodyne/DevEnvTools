e5900b9b0d55eec20d3fe56f6906910e435d3718
Stefan Schablowski <stefan.schablowski@desmodyne.com>
2017-05-24 18:31:43 +0200

    [#4]: unit tests call script not on python module, but on shell level

--- code/python/test_parse_ssh_conn_string.py
+++ code/python/test_parse_ssh_conn_string.py
@@ -4,7 +4,7 @@
 '''
 test_parse_ssh_conn_string.py
 
-unit-test script to parse ssh connection string and print its components
+unit test for parse_ssh_conn_string.py
 
 author  : stefan schablowski
 contact : stefan.schablowski@desmodyne.com
@@ -12,80 +12,93 @@ created : 2017-05-22
 '''
 
 
-from parse_ssh_conn_string import parse_conn_str
+from subprocess import check_output
+from unittest   import main, TestCase
 
-from unittest import main, TestCase
+
+# TODO: check for subprocess.CalledProcessError not being raised
+# TODO: remove code duplication
+
+
+# test subject is a python script; call it as a shell script
+# anyway as this is the way that script is used in production
+test_subject = './parse_ssh_conn_string.py'
 
 
 class test_hostname(TestCase):
     def setUp(self):
-        self.test_string = 'hostname'
+        test_string = 'hostname'
+        self.lines  = check_output([test_subject, test_string]).splitlines()
     def test_username(self):
-        self.assertEqual(parse_conn_str(self.test_string)[0], '')
+        self.assertEqual(self.lines[0], '')
     def test_password(self):
-        self.assertEqual(parse_conn_str(self.test_string)[1], '')
+        self.assertEqual(self.lines[1], '')
     def test_hostname(self):
-        self.assertEqual(parse_conn_str(self.test_string)[2], 'hostname')
+        self.assertEqual(self.lines[2], 'hostname')
     def test_port(self):
-        self.assertEqual(parse_conn_str(self.test_string)[3], '')
+        self.assertEqual(self.lines[3], '')
     def test_path(self):
-        self.assertEqual(parse_conn_str(self.test_string)[4], '')
+        self.assertEqual(self.lines[4], '')
 
 class test_username_hostname(TestCase):
     def setUp(self):
-        self.test_string = 'username@hostname'
+        test_string = 'username@hostname'
+        self.lines  = check_output([test_subject, test_string]).splitlines()
     def test_username(self):
-        self.assertEqual(parse_conn_str(self.test_string)[0], 'username')
+        self.assertEqual(self.lines[0], 'username')
     def test_password(self):
-        self.assertEqual(parse_conn_str(self.test_string)[1], '')
+        self.assertEqual(self.lines[1], '')
     def test_hostname(self):
-        self.assertEqual(parse_conn_str(self.test_string)[2], 'hostname')
+        self.assertEqual(self.lines[2], 'hostname')
     def test_port(self):
-        self.assertEqual(parse_conn_str(self.test_string)[3], '')
+        self.assertEqual(self.lines[3], '')
     def test_path(self):
-        self.assertEqual(parse_conn_str(self.test_string)[4], '')
+        self.assertEqual(self.lines[4], '')
 
 class test_username_password_hostname(TestCase):
     def setUp(self):
-        self.test_string = 'username:password@hostname'
+        test_string = 'username:password@hostname'
+        self.lines  = check_output([test_subject, test_string]).splitlines()
     def test_username(self):
-        self.assertEqual(parse_conn_str(self.test_string)[0], 'username')
+        self.assertEqual(self.lines[0], 'username')
     def test_password(self):
-        self.assertEqual(parse_conn_str(self.test_string)[1], 'password')
+        self.assertEqual(self.lines[1], 'password')
     def test_hostname(self):
-        self.assertEqual(parse_conn_str(self.test_string)[2], 'hostname')
+        self.assertEqual(self.lines[2], 'hostname')
     def test_port(self):
-        self.assertEqual(parse_conn_str(self.test_string)[3], '')
+        self.assertEqual(self.lines[3], '')
     def test_path(self):
-        self.assertEqual(parse_conn_str(self.test_string)[4], '')
+        self.assertEqual(self.lines[4], '')
 
 class test_username_password_hostname_port(TestCase):
     def setUp(self):
-        self.test_string = 'username:password@hostname:1234'
+        test_string = 'username:password@hostname:1234'
+        self.lines  = check_output([test_subject, test_string]).splitlines()
     def test_username(self):
-        self.assertEqual(parse_conn_str(self.test_string)[0], 'username')
+        self.assertEqual(self.lines[0], 'username')
     def test_password(self):
-        self.assertEqual(parse_conn_str(self.test_string)[1], 'password')
+        self.assertEqual(self.lines[1], 'password')
     def test_hostname(self):
-        self.assertEqual(parse_conn_str(self.test_string)[2], 'hostname')
+        self.assertEqual(self.lines[2], 'hostname')
     def test_port(self):
-        self.assertEqual(parse_conn_str(self.test_string)[3], '1234')
+        self.assertEqual(self.lines[3], '1234')
     def test_path(self):
-        self.assertEqual(parse_conn_str(self.test_string)[4], '')
+        self.assertEqual(self.lines[4], '')
 
 class test_username_password_hostname_port_path(TestCase):
     def setUp(self):
-        self.test_string = 'username:password@hostname:1234/path'
+        test_string = 'username:password@hostname:1234/path'
+        self.lines  = check_output([test_subject, test_string]).splitlines()
     def test_username(self):
-        self.assertEqual(parse_conn_str(self.test_string)[0], 'username')
+        self.assertEqual(self.lines[0], 'username')
     def test_password(self):
-        self.assertEqual(parse_conn_str(self.test_string)[1], 'password')
+        self.assertEqual(self.lines[1], 'password')
     def test_hostname(self):
-        self.assertEqual(parse_conn_str(self.test_string)[2], 'hostname')
+        self.assertEqual(self.lines[2], 'hostname')
     def test_port(self):
-        self.assertEqual(parse_conn_str(self.test_string)[3], '1234')
+        self.assertEqual(self.lines[3], '1234')
     def test_path(self):
-        self.assertEqual(parse_conn_str(self.test_string)[4], '/path')
+        self.assertEqual(self.lines[4], '/path')
 
 
 if __name__ == '__main__':
